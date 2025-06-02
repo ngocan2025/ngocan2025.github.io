18ed3aad8d052b74bc6b010e903b6545926f7c1f
ngocan2025 <tinhockienchu@yahoo.com>
2025-06-02 10:31:27 +0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/ngoc.jpg") {
+    myImage.setAttribute("src", "images/ngoc.jpg");
+  } else {
+    myImage.setAttribute("src", "images/an.jpg");
+  }
+});
+
+//proccessing button change user
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name: ");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `JavaScript is so cool ,${myName}`;
+  }
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
